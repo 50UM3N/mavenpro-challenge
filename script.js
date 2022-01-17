d13570fb9d80e103de6d8fd857f8f9f498cc42af
Soumen Khara <soumen2015.s.k@gmail.com>
2022-01-17 16:03:40 +0530

    fix: Fixed the instruction

--- script.js
+++ script.js
@@ -199,8 +199,8 @@ preloadImages(
     () => {
         document.querySelector(".spinner-wrapper").remove();
         if (confirm("Start the rating process") == true) {
-            // displayInstruction();
-            startRating();
+            displayInstruction();
+            // startRating();
         } else {
             const para = document.createElement("p");
             para.classList.add("game-end");
