61779afaf965b62fee06044d112342b6e18659f6
Soumen Khara <soumen2015.s.k@gmail.com>
2022-01-21 12:49:09 +0530

    core: Init the react with webpack, babble, eslint

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const ESLintPlugin = require("eslint-webpack-plugin");
+module.exports = (env) => {
+    const isProduction = env === "production";
+    return {
+        entry: "./src/index.jsx",
+        output: {
+            filename: "bundle.[fullhash].js",
+            path: path.resolve(__dirname, "dist"),
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: "./public/index.html",
+            }),
+            new ESLintPlugin({
+                extensions: ["ts", "js", "jsx"],
+            }),
+        ],
+        resolve: {
+            modules: [__dirname, "src", "node_modules"],
+            extensions: ["*", ".js", ".jsx", ".tsx", ".ts"],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.jsx?$/,
+                    exclude: /node_modules/,
+                    use: ["babel-loader"],
+                },
+                {
+                    test: /\.css$/,
+                    use: ["style-loader", "css-loader"],
+                },
+                {
+                    test: /\.png|svg|jpg|gif$/,
+                    use: ["file-loader"],
+                },
+            ],
+        },
+        devtool: isProduction ? "source-map" : "inline-source-map",
+    };
+};
